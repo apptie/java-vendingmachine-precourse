8a896b16e5206f3b520adbf40062b8d9f8acc00e
apptie <appti@naver.com>
2022-12-04 17:59:07 +0900

    feat(MachineInfo): 자판기의 정보를 관리하는 기능 추가

---
+++ src/main/java/vendingmachine/domain/machine/MachineInfo.java
@@ -0,0 +1,32 @@
+package vendingmachine.domain.machine;
+
+import java.util.Map;
+import vendingmachine.domain.coin.Coin;
+import vendingmachine.domain.product.Products;
+import vendingmachine.utils.coin.CoinGenerator;
+
+public class MachineInfo {
+
+    private final Balance balance;
+    private Products products;
+
+    public MachineInfo(int chargeAmount, CoinGenerator generator) {
+        this.balance = new Balance(chargeAmount, generator);
+    }
+
+    public void registryProducts(String productInput) {
+        products = new Products(productInput);
+    }
+
+    public int purchaseProduct(String productName, int money) {
+        return products.purchaseProduct(productName, money);
+    }
+
+    public boolean isCanPurchaseAnything(int money) {
+        return products.isCanPurchaseAnything(money);
+    }
+
+    public Map<Coin, Integer> calculateCustomerBalance(int balanceAmount) {
+        return balance.calculateBalance(balanceAmount);
+    }
+}
