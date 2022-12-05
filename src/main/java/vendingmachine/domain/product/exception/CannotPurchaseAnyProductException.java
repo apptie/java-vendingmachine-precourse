325928b3a5390ac2d67597c0e1ed4397bcd299b5
apptie <appti@naver.com>
2022-12-05 14:59:30 +0900

    chore: 사용하지 않는 Custom Exception 삭제

--- src/main/java/vendingmachine/domain/product/exception/CannotPurchaseAnyProductException.java
+++
@@ -1,8 +0,0 @@
-package vendingmachine.domain.product.exception;
-
-public class CannotPurchaseAnyProductException extends RuntimeException {
-
-    public CannotPurchaseAnyProductException() {
-        super("어떠한 상품도 구매할 수 없습니다.");
-    }
-}
